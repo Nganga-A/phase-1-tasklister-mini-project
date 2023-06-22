fee53b683a9bcc31f6a6ecf2971d231412f43094
Abed Nganga <ngangaabed@gmail.com>
2023-06-22 14:27:19 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('task-description');
+  const taskList = document.getElementById('tasks');
+
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (taskInput.value.trim() !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskInput.value;
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.addEventListener('click', () => {
+        taskList.removeChild(taskItem);
+      });
+
+
+      taskList.appendChild(taskItem);
+      taskItem.appendChild(deleteBtn);
+      taskInput.value = '';
+
+    }
+    event.target.reset()
+  });
+
 });
+
+
